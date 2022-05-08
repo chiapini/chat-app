d197e87f06308ae2ae328a6fa2f16e045b2e3a58
chiapini <chiara.rei.filippini@gmail.com>
2022-05-08 09:09:11 +0900

    ユーザー認証機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2022_05_06_090258) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
